d948e9dd7de8ff74ae31013ecf6d6b7541cb571e
Ronaldo2017 <Ronaldo2017@users.noreply.github.com>
2022-03-10 11:03:36 -0300

    Apagado duplicação de componente

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -13,15 +13,6 @@ function MovieCard() {
         
     };
 
-    const movie2 = {
-        id: 1,
-        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
-        title: "The Witcher",
-        count: 2,
-        score: 4.5
-        
-    };
-
     return (
         <div>
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
