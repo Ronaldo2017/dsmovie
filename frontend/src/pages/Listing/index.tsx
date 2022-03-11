52647fa9c5c4e7dad384bc00684d62d94eabd005
Ronaldo2017 <Ronaldo2017@users.noreply.github.com>
2022-03-11 08:03:15 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -2,18 +2,40 @@ import axios from "axios";
 import MovieCard from "../../components/MovieCard";
 import Pagination from "../../components/Pagination";
 import { BASE_URL } from "utils/requests";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 
 
 function Listing() {
 
-    //teste de requisicao(errada)
-    axios.get(`${BASE_URL}/movies?size=12page=0`)
+    //requisição correta com ReactHooks
+    //Hooks são funções cujo comportamento está vinculado ao estado e ao ciclo de vida do React a partir de componentes funcionais.
+
+    //fazendo requisição com useState
+    const [pageNumber, setPageNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12page=1`)
         .then(response =>{
+            const data = response.data as MoviePage
+            setPageNumber(data.number);
             console.log(response.data)
-        })
+          })
+    }, [])
+    
+    //teste de requisicao(errada)/roda mais de uma vez
+   // axios.get(`${BASE_URL}/movies?size=12page=1`)
+    //  .then(response =>{
+     //     const data = response.data as MoviePage
+       //   setPageNumber(data.number);
+      //    console.log(response.data)
+     //   })
+
+
 
     return (
         <>
+        <p>{pageNumber}</p>
             <Pagination />
             <div className="container">
                 <div className="row">
